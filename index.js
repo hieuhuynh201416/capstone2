5ae5abe4eeb3384ae70ff85da7286c0ef0464b59
hieuhuynh201416 <hieuhuynh201416@gmail.com>
2023-11-13 15:19:22 +0700

    update filter

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const imgNumber = document.querySelectorAll(".slider-content-left-top img")
+const imgNumber = document.querySelectorAll(".slider-content-left-top img");
 let index = 0;
-const rightbtn = document.querySelector('.fa-chevron-right')
-const leftbtn = document.querySelector('.fa-chevron-left')
-rightbtn.addEventListener("click",function(){
-index = index +1;
-if(index>imgNumber.length-1){
-  index = 0
-}
-  document.querySelector(".slider-content-left-top").style.right = index *100 + "%"
-})
-leftbtn.addEventListener("click",function(){
-  index = index -1;
-if(index<=0){
-  index = imgNumber.length -1
-}
-  document.querySelector(".slider-content-left-top").style.right = index *100 + "%"
-})
-
+const rightbtn = document.querySelector(".fa-chevron-right");
+const leftbtn = document.querySelector(".fa-chevron-left");
+rightbtn.addEventListener("click", function () {
+  index = index + 1;
+  if (index > imgNumber.length - 1) {
+    index = 0;
+  }
+  document.querySelector(".slider-content-left-top").style.right =
+    index * 100 + "%";
+});
+leftbtn.addEventListener("click", function () {
+  index = index - 1;
+  if (index <= 0) {
+    index = imgNumber.length - 1;
+  }
+  document.querySelector(".slider-content-left-top").style.right =
+    index * 100 + "%";
+});
 
 // Lớp đối tượng Product
 class Product {
@@ -56,16 +57,20 @@ function displayProducts(products) {
     productDiv.innerHTML = `
             <div><img src="${product.img}" alt="${product.name}"></div>
             <!-- Hiển thị thông tin sản phẩm -->
-            <div class="product-name-container"  style="text-align: center"> <h3>${product.name}</h3></div>
+            <div class="product-name-container"  style="text-align: center"> <h3>${
+              product.name
+            }</h3></div>
             <div class="product-detail-container">
             <p style="color:red; font-size: 30px" > $${product.price}</p>
             <p><strong>Màn hình:</strong> ${product.screen}</p>
             <p><strong>Máy ảnh sau:</strong> ${product.backCamera} pixel</p>
             <p><strong>Máy ảnh trước:</strong> ${product.frontCamera} pixel</p>
             <p><strong>Mô tả:</strong>${product.desc}</p>
-            <p><strong>Loại:</strong> ${product.type ? 'Samsung' : 'iPhone'}</p></div>
+            <p><strong>Loại:</strong> ${
+              product.type ? "Samsung" : "iPhone"
+            }</p></div>
         `;
-       
+
     productList.appendChild(productDiv);
   });
 }
@@ -88,5 +93,53 @@ fetch("https://653122e04d4c2e3f333c71f9.mockapi.io/capstone2")
           item.type
         )
     );
+    displayProducts(products);
+  });
+
+// Lọc sản phẩm theo loại
+function filterProducts(type) {
+  const productItems = document.querySelectorAll(
+    ".slider-product-one-content-item"
+  );
+
+  productItems.forEach((product) => {
+    const productType = product.dataset.type;
+
+    if (type === "All" || productType === type) {
+      product.style.display = "block"; // Hiển thị sản phẩm thuộc loại được chọn hoặc tất cả sản phẩm
+    } else {
+      product.style.display = "none"; // Ẩn các sản phẩm không thuộc loại được chọn
+    }
+  });
+}
+
+// Bắt sự kiện click cho nút lọc
+const filterButtons = document.querySelectorAll(".filterButton");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const type = button.dataset.type;
+    filterProducts(type);
+  });
+});
+
+// Gọi API MockAPI để lấy danh sách sản phẩm và hiển thị
+fetch("https://653122e04d4c2e3f333c71f9.mockapi.io/capstone2")
+  .then((response) => response.json())
+  .then((data) => {
+    const products = data.map((item) => {
+      return new Product(
+        item.id,
+        item.name,
+        item.price,
+        item.screen,
+        item.backCamera,
+        item.frontCamera,
+        item.img,
+        item.desc,
+        item.type
+      );
+    });
+
     displayProducts(products);
   });
